64037c93796ea74f27448fa8181ba05a6ae56732
Nicolás <140818402+Athan98@users.noreply.github.com>
2024-06-22 17:15:50 -0300

    Update front gestion_turnos

--- Desarrollo web/gestion_turnos/js/nuevoPaciente.js
+++ Desarrollo web/gestion_turnos/js/nuevoPaciente.js
@@ -1,2 +1,20 @@
 //Declaracion de variables
 const url = "http://192.168.1.8:8080/pacientes/crear";
+
+let formulario=document.querySelector("#datosFormulario");
+
+formulario.addEventListener("submit",e=>{
+    e.preventDefault();
+
+    //Obtener datos del form
+    let datos=new FormData(formulario);
+
+    peticion={
+        method:"POST",
+        body:datos
+    }
+
+    
+})
+
+console.log(formulario)
